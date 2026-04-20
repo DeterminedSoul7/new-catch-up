b4d49f833a1dec4f7878fa74695727f00bc60d9d
Renzo Bojorquez <u202411402@upc.edu.pe>
2026-04-20 12:37:30 -0500

    feat(app): add source for entity representation.

---
+++ src/news/domain/source.entity.js
@@ -0,0 +1,11 @@
+export class Source {
+    constructor(id = '', description = '', url = '', category = '', language = '', country = '') {
+        this.id = id;
+        this.description = description;
+        this.url = url;
+        this.category = category;
+        this.language = language;
+        this.country = country;
+        this.urlToLogo = '';
+    }
+}
