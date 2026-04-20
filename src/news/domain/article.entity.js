ac4e43cdce8dfd20fbda27e6b5e1c6314e4b57ad
Renzo Bojorquez <u202411402@upc.edu.pe>
2026-04-20 12:37:49 -0500

    feat(app): create article entity with classes and formatting method.

---
+++ src/news/domain/article.entity.js
@@ -0,0 +1,22 @@
+export class Article {
+    constructor({ title = '', description = '',
+                    url = '', urlToImage = '', source = null,
+                    publishedAt = '' } ) {
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.source = source;
+        this.publishedAt = publishedAt;
+    }
+
+    getFormattedPublishedAt() {
+        return this.publishedAt.toLocaleDateString( 'en-US', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'
+        });
+    }
+}
